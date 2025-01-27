c9fad66c9fb3d4251c882f65aefe6fd425816d95
Markus Lång <markus.lang@aalto.fi>
2025-01-27 15:38:04 +0200

    set up esp-idf for firmware development

---
+++ firmware/main/main.c
@@ -0,0 +1,43 @@
+#include <driver/gpio.h>
+#include <freertos/FreeRTOS.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
+#include <esp_log.h>
+#include <math.h>
+
+// Private config should include the defenitions:
+// WIFI_SSID
+// WIFI_PASSWORD
+// SERVER_IP
+// SERVER_PORT
+#include "spi_master.h"
+
+#define LED_PIN GPIO_NUM_14
+#define TAG "main"
+
+void app_main(void) {
+    // IO configuration setup
+    gpio_config_t io_config = {
+        .pin_bit_mask = (1ULL << LED_PIN), // 64 bit mask
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    }; 
+
+    gpio_config(&io_config); // Pass gpio_config_t pointer to load the config
+    
+    int t = 0;
+    int running = 1;
+
+    while(running) {
+        gpio_set_level(LED_PIN , 1);
+        printf("LED on\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        gpio_set_level(LED_PIN, 0);
+        printf("LED off\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        t++;
+    }
+}
+
---
+++ firmware/main/spi_master.h
@@ -0,0 +1,29 @@
+// This file contains SPI master driver configuration and API for communicating with 
+// LSM6DS3TR accelerometer and gyroscope SoC.
+
+#pragma once
+
+#include <esp_err.h>
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+#include "driver/spi_common.h"
+#include "hal/spi_types.h"
+
+#define INT_1   GPIO_NUM_9
+#define INT_2   GPIO_NUM_14
+#define SDO     GPIO_NUM_10
+#define SDA     GPIO_NUM_11
+#define SCL     GPIO_NUM_12
+#define CS      GPIO_NUM_13
+
+#define SPI_SCL_SPEED 1e6 // 1MHz clock out
+
+// Initalize the SPI driver with given config
+esp_err_t init_spi(spi_bus_config_t config);
+
+// Transmit byte array through spi and save response in rec
+esp_err_t spi_transmit_bytes(uint8_t *bytes, uint8_t *rec);
+
+// Deinitialize the SPI driver.
+esp_err_t deinit_spi(void);
+
