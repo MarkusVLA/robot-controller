27d4f6988d84fd2a507f2729a7abaaa56d60ba44
Markus Lång <markusvlang@gmail.com>
2025-01-30 16:27:38 +0200

    Connection to wifi and simple test code in main

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -3,15 +3,21 @@
 #include <esp_event.h>
 #include <nvs_flash.h>
 #include <esp_log.h>
-#include <math.h>
 
-#include "spi_master.h"
+#include "wifi_api.h"
+#include "rest_api.h"
+
+// Private config should include the defenitions:
+// WIFI_SSID
+// WIFI_PASSWORD
+// SERVER_IP
+// SERVER_PORT
+#include "private_config.h" 
 
 #define LED_PIN GPIO_NUM_14
 #define TAG "main"
 
 void app_main(void) {
-    // IO configuration setup
     gpio_config_t io_config = {
         .pin_bit_mask = (1ULL << LED_PIN), // 64 bit mask
         .mode = GPIO_MODE_OUTPUT,
@@ -20,19 +26,29 @@ void app_main(void) {
         .intr_type = GPIO_INTR_DISABLE
     }; 
 
-    gpio_config(&io_config); // Pass gpio_config_t pointer to load the config
-    
-    int t = 0;
-    int running = 1;
+    gpio_config(&io_config);
 
-    while(running) {
-        gpio_set_level(LED_PIN , 1);
-        printf("LED on\n");
-        vTaskDelay(pdMS_TO_TICKS(1000));
-        gpio_set_level(LED_PIN, 0);
-        printf("LED off\n");
-        vTaskDelay(pdMS_TO_TICKS(1000));
-        t++;
+    init_wifi();
+    esp_err_t ret = connect_wifi(WIFI_SSID, WIFI_PASSWORD);
+
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to connect to WiFi");
+        return;
     }
+
+    esp_netif_ip_info_t ip_info;
+    esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_STA"), &ip_info);
+    ESP_LOGI(TAG, "Server running on address: " IPSTR, IP2STR(&ip_info.ip));
+
+    // Init REST API
+    httpd_handle_t server_handle = start_webserver();
+
+    int running = 1;
+    while(running) {
+        vTaskDelay(pdMS_TO_TICKS(8000));
+    }
+
+    stop_webserver(server_handle);
+    deinit_wifi();
 }
 
